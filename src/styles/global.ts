b0806edfc8b06f2eaebd8c746e3714a1b9100429
brunoBMonteiro <brunobilheri@hotmail.com>
2021-03-16 15:41:17 -0300

    componente summary concluido, estrutura estilização

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
   --background: #F0F2F5;
   --red: #E52E4D;
+  --green: #33CC95;
   --blue: #5429CC;
   --blue-light: #6933FF;
   --text-title: #363F5F;
@@ -32,6 +33,15 @@ html {
 body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
 }
 
 button {
