2fa34b4b6483d1d7ad4bbb8421e7b4c3176c91c5
brunoBMonteiro <brunobilheri@hotmail.com>
2021-03-17 14:22:11 -0300

    criando e estilizando o componente da table

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,39 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td>R$12.800</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td>R$12.800</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td>R$12.800</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+}
---
+++ src/components/TransactionsTable/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin-top: 4rem;
+
+    table {
+        width: 100%;
+        border-spacing: 0 0.5rem;
+
+        th{
+            color: var(--text-body);
+            font-weight: 400;
+            padding: 1rem 2rem;
+            text-align: left;
+            line-height: 1.5rem;
+        }
+    }
+`;
