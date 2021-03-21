c7ed0a3b490fedcfc8fe0afe68d319945e850650
brunoBMonteiro <brunobilheri@hotmail.com>
2021-03-21 14:21:13 -0300

    Introdução á contextos, e context api

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,14 @@
+import React, { useContext } from 'react';
 import incomeImg from '../../assets/entradas.svg'
 import outcomeImg from '../../assets/saidas.svg'
 import totalImg from '../../assets/total.svg'
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from "./styles";
 
 export function Summary() {
+    const data = useContext(TransactionsContext);
+
     return (
         <Container>
             <div>
