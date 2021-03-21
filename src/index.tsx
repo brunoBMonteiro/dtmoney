05aa57408de4579d6e64503b300b93110a9b09e4
brunoBMonteiro <brunobilheri@hotmail.com>
2021-03-21 00:21:01 -0300

    listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Frelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1200,
+          createdAt: new Date('2021-02-18 10:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
