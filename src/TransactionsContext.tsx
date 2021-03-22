2bb5a77cc5e9e5a43a1568fa670d1a78da7ced74
brunoBMonteiro <brunobilheri@hotmail.com>
2021-03-21 21:21:39 -0300

    inserçãofinalizada

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -35,8 +35,17 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, []);
 
-    async function createTransaction(transaction: TransactionInput) {
-        await api.post('/transactions', transaction)
+    async function createTransaction(transactionInput: TransactionInput) {
+       const response = await api.post('/transactions',{
+            ...transactionInput,
+            createdAt: new Date(),
+        })
+       const { transaction } = response.data;
+
+       setTransactions([
+            ...transactions,
+            transaction,
+       ]);
     }
 
     return (
