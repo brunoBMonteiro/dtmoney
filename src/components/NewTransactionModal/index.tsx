617743834ab3eb5b924a2cf4395fcc3bd1425953
brunoBMonteiro <brunobilheri@hotmail.com>
2021-03-21 22:59:30 -0300

    criando hooks

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,13 +1,12 @@
-import { FormEvent, useState, useContext } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
-import { TransactionsContext } from '../../TransactionsContext';
-import { api } from '../../services/api';
 
 import incomeImg from '../../assets/entradas.svg';
 import outcomeImg from '../../assets/saidas.svg';
 import closeImg from '../../assets/fechar.svg';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import {useTransactions} from '../../hooks/useTransactions';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -15,7 +14,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps ) {
-    const {createTransaction} = useContext(TransactionsContext);
+    const {createTransaction} = useTransactions();
     
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,13 +1,12 @@
-import { useContext } from 'react';
 import incomeImg from '../../assets/entradas.svg'
 import outcomeImg from '../../assets/saidas.svg'
 import totalImg from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container } from "./styles";
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext);
+    const { transactions } = useTransactions();
 
     //const totalDeposits = transactions.reduce((acc, transaction) => {
     //   if (transaction.type === 'deposit') {
